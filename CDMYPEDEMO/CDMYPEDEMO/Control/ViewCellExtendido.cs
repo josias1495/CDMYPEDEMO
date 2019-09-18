55c3980406d4d09cacca45ebc4fc3c8b4e456560
DELL <DELL@DELL-PC>
2019-09-17 19:21:35 -0600

    Agregar archivos de proyecto.

---
+++ CDMYPEDEMO/CDMYPEDEMO/Control/ViewCellExtendido.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CDMYPEDEMO.Control
+{
+    public class ViewCellExtendido : ViewCell
+    {
+        public static readonly BindableProperty SeleccionBackGroundColorProperty = BindableProperty.Create(
+            "SeleccionBackGroundColorProperty", typeof(Color), typeof(ViewCellExtendido), Color.Default);
+
+        public Color SeleccionBackGroundColor
+        {
+            get { return (Color)GetValue(SeleccionBackGroundColorProperty); }
+            set { SetValue(SeleccionBackGroundColorProperty, value); }
+        }
+    }
+}
